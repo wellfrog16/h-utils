709a48d98977aec5857e21d8ea68726abb9d5326
wellfrog <wellfrog16@gmail.com>
2021-06-28 02:26:20 +0800

    增加rules模块

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as filterTree } from './filterTree';
 export { default as file } from './file';
 export { default as storage, IStorageGetOption, IStorageSetOption, IStorageData } from './storage';
 export { default as loader, ICDNType, RawEditorSettings } from './loader';
+export { default as rules, IRuleOptions } from './rules';
---
+++ src/rules/index.ts
@@ -0,0 +1,58 @@
+export interface IRuleOptions {
+    name: string;
+    allowEmpty: boolean; // allowEmpty: 允许为空(false)
+    allowSpecial: boolean; // allowSpecial: 允许特殊字符(true)
+    trigger: 'change' | 'blur';
+    message: string;
+}
+
+const rules = {
+    /**
+     * 自带类型校验
+     *
+     * @param {String} key 校验的字段
+     * @param {JSON} [options={}]
+     * @returns
+     */
+    check(key: string, options = {}): Record<string, unknown[]> {
+        const params = { required: true, trigger: 'blur', ...options };
+        const r: Record<string, unknown[]> = {};
+        r[key] = [{ ...params }];
+        return r;
+    },
+
+    /**
+     * 校验字符串
+     *
+     * @param {*} key
+     * @param {*} [options={}]
+     * @returns
+     */
+    checkString(key: string, options?: Partial<IRuleOptions>) {
+        const params: Partial<IRuleOptions> = {
+            name: '此项',
+            allowEmpty: false,
+            allowSpecial: true,
+            trigger: 'change',
+            ...options,
+        };
+        const { name, message, trigger } = params;
+        const r: Record<string, Record<string, unknown>[]> = {};
+        const rule: Record<string, unknown>[] = [];
+
+        // 是否允许特殊字符，待扩展
+        if (!params.allowSpecial) {
+            rule.push({ pattern: /^[\u4e00-\u9fa5|a-z|A-Z]+$/, message: `${name}只可中文/英文/拼音，不可特殊符号，数字`, trigger });
+        }
+
+        // 是否允许为空
+        if (!params.allowEmpty) {
+            rule.push({ required: true, message: (message || `请输入${name}`), trigger });
+        }
+
+        r[key] = rule;
+        return r;
+    },
+};
+
+export default rules;
